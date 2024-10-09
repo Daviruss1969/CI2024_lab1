aa81ec405bbb7b03d7578f5f4da30f173bf3854c
Daviruss1969 <lukasloiodic@gmail.com>
2024-10-09 11:23:41 +0200

    [FEAT] Implement the algorithm in the .py file

--- set_cover.py
+++ set_cover.py
@@ -5,10 +5,25 @@ UNIVERSES_SIZE = [100, 1000, 10000, 100000, 100000, 100000]
 NUMS_SETS = [10, 100, 1000, 10000, 10000, 10000]
 DENSITIES = [0.2, 0.2, 0.2, 0.1, 0.2, 0.3]
 
-# Consts used to define boundary to the problem (can be edited)
-MAXS_ITERATIONS = [50000, 5000, 500, 50, 50, 50]
-MAXS_EXPLORATIONS = [15, 10, 5, 2, 2, 2]
+# Initialisation
+print('Select your instance for the set cover problem :')
+print('0) Universe size :100              | Num sets :10                | Density:0.2')
+print('1) Universe size :1 000            | Num sets :100               | Density:0.2')
+print('2) Universe size :10 000           | Num sets :1 000             | Density:0.2')
+print('3) Universe size :100 000          | Num sets :10 000            | Density:0.1')
+print('4) Universe size :100 000          | Num sets :10 000            | Density:0.2')
+print('5) Universe size :100 000          | Num sets :10 000            | Density:0.3')
 
+index = int(input("Enter problem number (e.g.: 0) : "))
+
+UNIVERSE_SIZE = UNIVERSES_SIZE[index]
+NUM_SETS = NUMS_SETS[index]
+DENSITY = DENSITIES[index]
+SETS = np.random.random((NUM_SETS, UNIVERSE_SIZE)) < DENSITY
+for s in range(UNIVERSE_SIZE):
+    if not np.any(SETS[:, s]):
+        SETS[np.random.randint(NUM_SETS), s] = True
+COSTS = np.pow(SETS.sum(axis=1), 1.1)
 
 def valid(solution: np.ndarray) -> bool:
     """Checks wether solution is valid (ie. covers all universe)"""
@@ -22,49 +37,66 @@ def fitness(solution: np.ndarray) -> tuple[bool, int]:
     """"Return the fitness of a solution (to be maximized)"""
     return (valid(solution), -cost(solution))
 
-def tweak(solution: np.ndarray, num_sets: int) -> np.ndarray:
-    """tweak function that perform a mutation on n random index according to a given probability"""
-    mask = np.random.random(num_sets) < 0.01
+def tweak(solution: np.ndarray, strength: float) -> np.ndarray:
+    """tweak function that perform a mutation on n random index according to a probability"""
+    mask = np.random.random(NUM_SETS) < strength
+    if not np.any(mask):
+        mask[np.random.randint(NUM_SETS)] = True
     new_sol = np.logical_xor(solution, mask)
     return new_sol
 
-def set_cover(initial_solution: np.ndarray, num_sets: int, max_iterations: int, max_explorations: int) -> np.ndarray:
+def set_cover(initial_solution: np.ndarray, max_iterations: int) -> np.ndarray:
     """Perform the set_cover algorithm on a given solution"""
     solution = initial_solution.copy()
-    for _ in range(max_iterations):
-        test_solution = tweak(solution, num_sets)
-        for _ in range(max_explorations):
-            new_solution = tweak(solution, num_sets)
-            if (fitness(new_solution) > fitness(test_solution)):
-                test_solution = new_solution
+    fitness_solution = fitness(solution)
+    strength = 0.3
+    succes = 0
+    c = 0.98
+    for i in range(max_iterations):
+        if i % (max_iterations // 10) == 0:
+            percent_complete = (i / max_iterations) * 100
+            print(f"\t{percent_complete:.0f}%")
 
-        if fitness(test_solution) > fitness(solution):
+        if i%5:
+            if succes > 1:
+                strength/c
+            elif succes < 1:
+                strength*=c
+            succes = 0
+
+        test_solution = tweak(solution, strength)
+        fitness_test_solution = fitness(test_solution)
+        if fitness_test_solution > fitness_solution:
             solution = test_solution
+            fitness_solution = fitness_test_solution
+            succes += 1
     return solution
 
-for i in range(6):
-    # Get the variables for the current problem
-    num_sets = NUMS_SETS[i]
-    universe_size = UNIVERSES_SIZE[i]
-    density = DENSITIES[i]
-    max_iterations = MAXS_ITERATIONS[i]
-    max_explorations = MAXS_EXPLORATIONS[i]
-
-    # Initialisation of the sets (transform them into a feasable problem if not)
-    SETS = np.random.random((num_sets, universe_size)) < density
-    for s in range(universe_size):
-        if not np.any(SETS[:, s]):
-            SETS[np.random.randint(num_sets), s] = True
-
-    # Compute costs
-    COSTS = np.pow(SETS.sum(axis=1), 1.1)
-
-    # Generate initial solutions
-    initial_solution = np.full(num_sets, True)
-
-    print(f"Performing the set_cover algorithm for num_sets={num_sets}, universe_size={universe_size}, density={density}, max iterations={max_iterations}, max explorations={max_explorations}")
-    print(f"The initial cost is {cost(initial_solution)}")
-
-    final_solution = set_cover(initial_solution, num_sets, max_iterations, max_explorations)
-    print(f"The final cost of the solution is {cost(final_solution)}")
+def multiple_set_cover(trials: int, max_iterations: int)-> np.ndarray:
+    """Perform multiple time a set_cover algorithm and return the best one"""
+    solution = np.random.random(NUM_SETS) < .5
+    for i in range(trials):
+        print(f'Trial number : {i+1}')
+        new_solution = np.random.random(NUM_SETS) < .5
+        new_solution = set_cover(new_solution, max_iterations)
+        if (fitness(new_solution) > fitness(solution)):
+            solution = new_solution
     print()
+    return solution
+
+max_iterations = input('Select your number of iteration for the problem (default 5 000) : ')
+if max_iterations == '':
+    max_iterations = 5_000
+else :
+    max_iterations = int(max_iterations)
+
+trials = input('Select your number of trials for the problem (default 5) : ')
+if trials == '':
+    trials = 5
+else :
+    trials = int(trials)
+
+print()
+solution = multiple_set_cover(trials, max_iterations)
+solution_fitness = fitness(solution)
+print(f'The solution is{"" if solution_fitness[0] else " not"} valid, the final cost is {-solution_fitness[1]}')
